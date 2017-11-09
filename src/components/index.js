8053ae9f99e66935709728f139ae1fd4bf336731
Vladimir Novick <vladimirnovick@Vladimirs-MacBook-Pro.local>
2017-11-09 10:35:45 -0600

    phase 2.1

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Message from './Message';
+import Compose from './Compose';
+
+export { 
+  Message,
+  Compose
+}
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,28 +1,72 @@
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { getMockData } from '../services/api';
+import { View, ImageBackground, Text, StyleSheet, Button, FlatList } from 'react-native';
 
 export default class ChatScreen extends React.Component {
 
-  static navigationOptions = ({ navigation }) => ({
-    title: `Chat with ${navigation.state.params.user}`
-  });
+    static navigationOptions = ({ navigation }) => ({
+        title: `Chat with ${navigation.state.params.user}`
+    })
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    state = {
+        messages: []
+    }
+
+    componentDidMount() {
+        getMockData().then((messages) => {
+            this.setState({
+                messages
+            })
+        });
+    }
+   
+    
+    getMessageRow(item) {
+        return (
+            <View style={[
+                styles.listItem, item.incoming ?
+                    styles.incomingMessage :
+                    styles.outgoingMessage
+            ]}>
+                <Text>{item.message}</Text>
+            </View>
+        )
+    } 
+
+    render() {
+        return (
+            <ImageBackground
+                style={styles.container} source={require('../assets/imgs/background.png')}> 
+                <FlatList
+                    data={this.state.messages}
+                    renderItem={({ item }) =>
+                        this.getMessageRow(item) 
+                    }
+                    keyExtractor={(item, index) => (`message-${index}`)}
+                />
+            </ImageBackground>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff'
-  }
-});
+    container: {
+        flex: 1,
+        backgroundColor: 'transparent', 
+        width: '100%'
+    },
+    listItem: {
+        width: '70%',
+        margin: 10,
+        padding: 10,
+        backgroundColor: 'white',
+        borderColor: '#979797',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderRadius: 10
+    },
+    incomingMessage: {
+        alignSelf: 'flex-end',
+        backgroundColor: '#E1FFC7'
+    } 
+})
